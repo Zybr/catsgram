6db8ce802ddd7d9b3e9c11e00a4cafda07656177
Mike <zybrof@gmail.com>
2025-08-23 10:21:40 +0300

    Post service

---
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PostService {
+    private final Map<Long, Post> posts = new HashMap<>();
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
+    public Post create(@RequestBody Post post) {
+        if (post.getDescription() == null || post.getDescription().isBlank()) {
+            throw new ConditionsNotMetException("Описание не может быть пустым");
+        }
+
+        post.setId(getNextId());
+        post.setPostDate(Instant.now());
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public Post update(@RequestBody Post update) {
+        if (update.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+
+        if (posts.containsKey(update.getId())) {
+            Post oldPost = posts.get(update.getId());
+
+            if (update.getDescription() == null || update.getDescription().isBlank()) {
+                throw new ConditionsNotMetException("Описание не может быть пустым");
+            }
+
+            oldPost.setDescription(update.getDescription());
+
+            return oldPost;
+        }
+
+        throw new NotFoundException("Пост с id = " + update.getId() + " не найден");
+    }
+
+    private long getNextId() {
+        long currentMaxId = posts.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+
+        return ++currentMaxId;
+    }
+}
